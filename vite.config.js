4c80f96c747daa599583eb4ba10623657242a740
Malaji Gopi Chand <thegopichand@gmail.com>
2026-03-29 04:47:41 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,8 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          { src: '/icon.svg', sizes: '192x192', type: 'image/svg+xml', purpose: 'any' },
-          { src: '/icon.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'maskable' }
+          { src: '/icon.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+          { src: '/icon.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
         ]
       }
     })
